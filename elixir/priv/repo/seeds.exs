a0f769668636c61c028ed295cac7ca6dd037bda1
davidjbarnes <david@onepointone.com>
2021-01-19 14:07:33 -0700

    1) seed the database

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -9,3 +9,36 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Homework.Merchants.Merchant
+alias Homework.Users.User
+alias Homework.Transactions.Transaction
+alias Homework.Repo
+
+merchant = Repo.insert!(%Merchant{
+    description: "David Merch is a merchant",
+    name: "David Merch"
+}, returning: true)
+
+user = Repo.insert!(%User{
+    first_name: "David",
+    last_name: "Barnes",
+    dob: "12/11/1979"
+}, returning: true)
+
+transactions = [
+  %Transaction{
+    amount: 1,
+    credit: true,
+    debit: false,
+    description: "candy bar",
+    merchant: merchant,
+    user: user
+  }
+]
+
+Enum.each(transactions, fn(transaction) ->
+  result = Repo.insert!(transaction, returning: true)
+  IO.inspect result
+end)
+
